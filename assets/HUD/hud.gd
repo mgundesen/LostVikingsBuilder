5773c945adff11b699be114381e1e16647413cee
Morten Gundesen <mortengundesen@gmail.com>
2025-07-22 11:20:55 +0200

    Make active selection in hud

--- assets/HUD/hud.gd
+++ assets/HUD/hud.gd
@@ -8,4 +8,6 @@ func _process(_delta):
 		for i in range(3):
 			var path = "H{player}_{health_index}".format({"player": index,"health_index": i+1})
 			get_node(path).visible = (i+1 <= health)
+		var path = "Active_{player}".format({"player": index})
+		get_node(path).visible = player.get("controlActive")
 		index += 1
