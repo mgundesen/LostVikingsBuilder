477711044fb7e03b9844a9996343f229dba22419
Morten Gundesen <mortengundesen@gmail.com>
2025-07-23 23:17:37 +0200

    fix double entry spring

--- assets/Interactive/spring.gd
+++ assets/Interactive/spring.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 var appliedSpring = false
 
+func unspring():
+	appliedSpring = false
+	$AnimatedSprite2D.play("Rest")
+
 func _on_body_entered(body: Node2D) -> void:
 	if !appliedSpring:
 		body.set("springJump", true)
 		appliedSpring = true
+		get_tree().create_timer(0.07).timeout.connect(func(): unspring())
 		$AnimatedSprite2D.play("Pressed")
-
-func _on_body_exited(_body: Node2D) -> void:
-	appliedSpring = false
-	$AnimatedSprite2D.play("Rest")
