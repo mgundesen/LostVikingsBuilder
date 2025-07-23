41b212051eb19fbe4b9938abf186040851521460
Morten Gundesen <mortengundesen@gmail.com>
2025-07-23 09:22:43 +0200

    Add invisible platform to top of ladder

--- assets/Interactive/ladder.gd
+++ assets/Interactive/ladder.gd
@@ -3,8 +3,9 @@ extends Area2D
 @export var ladderHeight = 1
 
 func _ready():
-	$CollisionShape2D.shape.size.y = ladderHeight
+	$CollisionShape2D.shape.size.y = ladderHeight + 1
 	$Sprite2D.region_rect.size.y = ladderHeight
+	$owplatform.position.y = -ladderHeight/2
 
 func _on_body_entered(body: Node2D) -> void:
 	body.set("ladderAllowed", true)
