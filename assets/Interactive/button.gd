6fe8f237c7019569fd471754c85c07c34da3877e
Morten Gundesen <mortengundesen@gmail.com>
2025-08-07 19:53:02 +0200

    Fix button collision with arrow and players

--- assets/Interactive/button.gd
+++ assets/Interactive/button.gd
@@ -5,12 +5,20 @@ class_name GameButton
 var pressed = false
 signal activated
 
+func buttonPress():
+	pressed = true
+	activated.emit()
+	get_tree().create_timer(0.1).timeout.connect(func(): pressed = false)
+
 func _process(_delta):
 	for body in get_overlapping_bodies():
 		if body is PlayerBase and body.get("controlActive") == true and Input.is_action_just_pressed(&"A"):
-			pressed = true
-			activated.emit()
-			get_tree().create_timer(0.1).timeout.connect(func(): pressed = false)
+			buttonPress()
+	for area in get_overlapping_areas():
+		if area is Hitbox:
+			buttonPress()
+			area.queue_free()
+			
 	if pressed:
 		$AnimatedSprite2D.play("pressed")
 	else:
