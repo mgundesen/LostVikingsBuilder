db1dc572d7e5b72f68191bd20815f380b7a725a8
Morten Gundesen <mortengundesen@gmail.com>
2025-08-09 17:46:51 +0200

    Change ladder interface for slightly more convenient usage

--- assets/Interactive/ladder.gd
+++ assets/Interactive/ladder.gd
@@ -1,17 +1,17 @@
 extends Area2D
 
-@export var ladderHeight = 1
+@export var ladderHeight = 4.0
 
 func _ready():
-	$CollisionShape2D.shape.size.y = ladderHeight + 1
-	$Sprite2D.region_rect.size.y = ladderHeight
-	$owplatform.position.y = -ladderHeight/2.0
+	$CollisionShape2D.shape.size.y = ladderHeight * 46 + 1
+	$Sprite2D.region_rect.size.y = ladderHeight * 46
+	$owplatform.position.y = -(ladderHeight * 46) /2.0
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerBase:
 		body.set("ladderAllowed", true)
 		body.set("ladderPos", position)
-		body.set("ladderHeight", ladderHeight)
+		body.set("ladderHeight", ladderHeight * 46)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is PlayerBase:
