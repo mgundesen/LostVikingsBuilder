cab1228f3375d81e9b173c3c05fd19826903bb34
Morten Gundesen <mortengundesen@gmail.com>
2025-08-24 08:04:59 +0200

    Update door with collision and operation modes

--- assets/Interactive/door.gd
+++ assets/Interactive/door.gd
@@ -1,16 +1,33 @@
-extends Area2D
+extends CharacterBody2D
 
 enum State{Open, Closed, Opening, Closing}
 var state = State.Closed
 
+enum Mode{Auto, Signals}
+@export var operationMode = Mode.Signals 
+
+func setCollision(on):
+	for layer in range(4):
+		set_collision_layer_value(layer+1, on)
+		set_collision_mask_value(layer+1, on)
+
+func open():
+	state = State.Open
+	setCollision(false)
+
+func close():
+	state = State.Closed
+	setCollision(true)
+
 func _process(_delta):
-	var close = PlayerUtil.closeToPlayer(position, 120)
-	if state == State.Closed and close:
-		state = State.Opening
-		get_tree().create_timer(0.07).timeout.connect(func(): state = State.Open)
-	if state == State.Open and !close:
-		state = State.Closing
-		get_tree().create_timer(0.07).timeout.connect(func(): state = State.Closed)
+	if operationMode == Mode.Auto:
+		var close = PlayerUtil.closeToPlayer(position, 120)
+		if state == State.Closed and close:
+			state = State.Opening
+			get_tree().create_timer(0.07).timeout.connect(func(): open())
+		if state == State.Open and !close:
+			state = State.Closing
+			get_tree().create_timer(0.07).timeout.connect(func(): close())
 	
 	if state == State.Closed:
 		$AnimatedSprite2D.play("default")
