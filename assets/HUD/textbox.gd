5d5918a24ef29b5c458574368a64e54dc14d90ac
Morten Gundesen <mortengundesen@gmail.com>
2025-08-30 13:43:27 +0200

    Initial textbox implementation without pause control

---
+++ assets/HUD/textbox.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+var textIndex = 0
+
+func _ready():
+	updateTextbox()
+
+func setText(color, text):
+	var stylebox = $MarginContainer/Panel.get_theme_stylebox("panel")
+	stylebox.bg_color =color
+	$MarginContainer/Panel.add_theme_stylebox_override("panel", stylebox)
+
+	$MarginContainer/MarginContainer/HBoxContainer/RichTextLabel.text = text
+
+func updateTextbox():
+	var textInfo = SceneControl.textForScene()
+	if len(textInfo) <= textIndex:
+		visible = false
+	else:
+		var line = textInfo[textIndex]
+		setText(line[0], line[1])
+
+func _process(_delta):
+	if Input.is_action_just_pressed(&"B"):
+		textIndex += 1
+		updateTextbox()
--- scene_control.gd
+++ scene_control.gd
@@ -2,19 +2,32 @@ extends Node
 
 var currentScene = 0
 
-var sceneList = ["res://assets/Menu/main_menu.tscn",
-				 "res://Level2/level2.tscn"]
+var eColor = Color8(148, 0, 0)
+var bColor = Color8(25, 121, 0)
+var oColor = Color8(107, 65, 33)
+
+var sceneList = [{"level" : "res://assets/Menu/main_menu.tscn"},
+				{"level" : "res://Level2/level2.tscn",
+				"text" : [[bColor, "Waking up in a cell again... this is starting to feel like a tradition."],
+						  [eColor, "Tradition? Feels more like a recurring side quest at this point."],
+						  [bColor, "Tradition or not, the guards actually remembered to lock our cell this time."],
+						  [oColor, "At least they also left some food for us, I'm starving"],
+						  [bColor, "Tradition or not, the guards actually remembered to lock our cell this time."],
+						  [eColor, "... Get busy living or get busy dying. Time to get busy escaping."]]}]
 
 func nextScene():
 	currentScene += 1
-	get_tree().change_scene_to_file(sceneList[currentScene])
+	get_tree().change_scene_to_file(sceneList[currentScene]["level"])
 
 func continueScene():
-	get_tree().change_scene_to_file(sceneList[currentScene])
+	get_tree().change_scene_to_file(sceneList[currentScene]["level"])
 	
 func mainScene():
 	currentScene = 0
-	get_tree().change_scene_to_file(sceneList[currentScene])
+	get_tree().change_scene_to_file(sceneList[currentScene]["level"])
 
 func deathScene():
 	get_tree().change_scene_to_file("res://assets/Menu/death_scene.tscn")
+	
+func textForScene():
+	return sceneList[currentScene]["text"]
