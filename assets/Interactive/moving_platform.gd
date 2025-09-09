b6f449ca8b3ffceb1c1057da5da450712babfb87
Morten Gundesen <mortengundesen@gmail.com>
2025-09-09 20:10:38 +0200

    Logic for platform moving circular
    Physics are doing weird stuff, test moving via velocity instead?

--- assets/Interactive/moving_platform.gd
+++ assets/Interactive/moving_platform.gd
@@ -1,20 +1,36 @@
 extends Node2D
 
+enum Mode {EndPoints, Circular}
+@export var mode = Mode.EndPoints
+# EndPoints mode
 var start = Vector2()
-@export var end = Vector2(100,100)
 @export var speed = 2.0
+@export var end = Vector2(100,100)
+#Circular Mode
+@export var radius = 200
+var center
+var angle = 0
 
 var target = Vector2()
 var nextTarget = Vector2()
 
 func _ready():
-	target = end + position
-	nextTarget = start + position
+	if mode == Mode.EndPoints:
+		target = end + position
+		nextTarget = start + position
+	else:
+		center = position
 
 func _physics_process(_delta):
-	position += position.direction_to(target) * speed
-	if position.distance_to(target) < speed:
-		# Why is swap not a thing in gd?
-		var temp = target
-		target = nextTarget
-		nextTarget = temp
+	if mode == Mode.EndPoints:
+		position += position.direction_to(target) * speed
+		if position.distance_to(target) < speed:
+			# Why is swap not a thing in gd?
+			var temp = target
+			target = nextTarget
+			nextTarget = temp
+	else:
+		angle += speed
+		var x = center.x + radius * cos(deg_to_rad(angle))
+		var y = center.y + radius * sin(deg_to_rad(angle))
+		position = Vector2(x,y)
