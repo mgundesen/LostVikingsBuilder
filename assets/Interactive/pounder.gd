bc5e49233e31a4685e441fe03bb969266f21f2f7
Morten Gundesen <mortengundesen@gmail.com>
2025-09-12 13:58:38 +0200

    Disable pounder while interacting with shield to avoid incorrect deaths

--- assets/Interactive/pounder.gd
+++ assets/Interactive/pounder.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const speed = 5
+var disabled = false
 
 var target = Vector2()
 var nextTarget = Vector2()
@@ -21,7 +22,13 @@ func _physics_process(_delta):
 		swapTarget()
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is PlayerBase:
+	if body is PlayerBase and disabled == false:
 		body.killSquash()
 	if body is OlafShield:
 		swapTarget()
+		disabled = true
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is OlafShield:
+		disabled = false
