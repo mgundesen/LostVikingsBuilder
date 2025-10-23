17f1476b13ccc17323ea9fc6c23867ccffcfe283
Morten Gundesen <mortengundesen@gmail.com>
2025-10-23 19:44:40 +0200

    Avoid inflater working while in air

--- assets/Interactive/inflater.gd
+++ assets/Interactive/inflater.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 func _process(_delta):
 	var player = PlayerUtil.getOverlappingActive(self)
-	if player and Input.is_action_just_pressed(&"A") and player.state == PlayerBase.State.Free:
+	var validPlayerState = player and player.state == PlayerBase.State.Free and player.is_on_floor()
+	if Input.is_action_just_pressed(&"A") and validPlayerState:
 		SceneControl.playSound($AudioStreamPlayer2D)
 		player.setState(PlayerBase.State.Inflating)
