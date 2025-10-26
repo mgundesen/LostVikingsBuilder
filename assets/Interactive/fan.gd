4fc1bf19e61ab2d280ece708c1d7a4a61696b7a3
Morten Gundesen <mortengundesen@gmail.com>
2025-10-26 18:01:44 +0100

    Add off state for fan

--- assets/Interactive/fan.gd
+++ assets/Interactive/fan.gd
@@ -4,11 +4,15 @@ extends CharacterBody2D
 @export var on = true
 
 func _process(_delta: float) -> void:
-	for body in $Area2D.get_overlapping_bodies():
-		if body is PlayerBase and body.state == PlayerBase.State.Inflated:
-			body.externalForce = pushForce
+	if on:
+		for body in $Area2D.get_overlapping_bodies():
+			if body is PlayerBase and body.state == PlayerBase.State.Inflated:
+				body.externalForce = pushForce
 
 	if on:
 		$AnimatedSprite2D.play("on")
 	else:
 		$AnimatedSprite2D.play("off")
+
+func toggle():
+	on = !on
