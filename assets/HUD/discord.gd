ea02f355113d7cbf90d86e30c7369b144521f3ce
Morten Gundesen <mortengundesen@gmail.com>
2026-01-04 21:04:08 +0100

    Add link to build engine

--- assets/HUD/discord.gd
+++ assets/HUD/discord.gd
@@ -1,4 +1,10 @@
 extends Button
 
+@export var iconPath = "res://assets/Menu/discordicon.png"
+@export var link = "https://discord.gg/DZ5mZ6EDC6"
+
+func _ready():
+	icon = load(iconPath)
+
 func _on_pressed() -> void:
-	OS.shell_open("https://discord.gg/DZ5mZ6EDC6")
+	OS.shell_open(link)
